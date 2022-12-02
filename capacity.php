e8c992823a0c8e5a8726470cad510da4e40baaaf
speedjin1995 <speedjin1995@gmail.com>
2022-12-02 11:39:28 +0800

    Initial commit

---
+++ capacity.php
@@ -0,0 +1,221 @@
+<?php
+require_once 'php/db_connect.php';
+
+session_start();
+
+if(!isset($_SESSION['userID'])){
+  echo '<script type="text/javascript">';
+  echo 'window.location.href = "login.html";</script>';
+}
+else{
+  $user = $_SESSION['userID'];
+}
+?>
+
+<div class="content-header">
+    <div class="container-fluid">
+        <div class="row mb-2">
+			<div class="col-sm-6">
+				<h1 class="m-0 text-dark">Capacity</h1>
+			</div><!-- /.col -->
+        </div><!-- /.row -->
+    </div><!-- /.container-fluid -->
+</div>
+<!-- /.content-header -->
+
+<!-- Main content -->
+<section class="content">
+	<div class="container-fluid">
+        <div class="row">
+			<div class="col-12">
+				<div class="card">
+					<div class="card-header">
+                        <div class="row">
+                            <div class="col-9"></div>
+                            <div class="col-3">
+                                <button type="button" class="btn btn-block bg-gradient-warning btn-sm" id="addCapacity">Add Capacity</button>
+                            </div>
+                        </div>
+                    </div>
+					<div class="card-body">
+						<table id="capacityTable" class="table table-bordered table-striped">
+							<thead>
+								<tr>
+                                    <th>No.</th>
+									<th>Capacity</th>
+									<th>Actions</th>
+								</tr>
+							</thead>
+						</table>
+					</div><!-- /.card-body -->
+				</div><!-- /.card -->
+			</div><!-- /.col -->
+		</div><!-- /.row -->
+	</div><!-- /.container-fluid -->
+</section><!-- /.content -->
+
+<div class="modal fade" id="capacityModal">
+    <div class="modal-dialog modal-xl">
+      <div class="modal-content">
+        <form role="form" id="capacityForm">
+            <div class="modal-header">
+              <h4 class="modal-title">Add Capacity</h4>
+              <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+              </button>
+            </div>
+            <div class="modal-body">
+              <div class="card-body">
+                <div class="form-group">
+                  <input type="hidden" class="form-control" id="id" name="id">
+                </div>
+                <div class="form-group">
+                  <label for="capacity">Capacity *</label>
+                  <input type="text" class="form-control" name="capacity" id="capacity" placeholder="Enter Capacity" required>
+                </div>
+              </div>
+            </div>
+            <div class="modal-footer justify-content-between">
+              <button type="button" class="btn btn-danger" data-dismiss="modal">Close</button>
+              <button type="submit" class="btn btn-primary" name="submit" id="submitMember">Submit</button>
+            </div>
+        </form>
+      </div>
+      <!-- /.modal-content -->
+    </div>
+    <!-- /.modal-dialog -->
+</div>
+
+<script>
+$(function () {
+    $("#capacityTable").DataTable({
+        "responsive": true,
+        "autoWidth": false,
+        'processing': true,
+        'serverSide': true,
+        'serverMethod': 'post',
+        'order': [[ 1, 'asc' ]],
+        'ajax': {
+            'url':'php/loadCapacity.php'
+        },
+        'columns': [
+            { data: 'counter' },
+            { data: 'capacity' },
+            { 
+                data: 'id',
+                render: function ( data, type, row ) {
+                    return '<div class="row"><div class="col-3"><button type="button" id="edit'+data+'" onclick="edit('+data+')" class="btn btn-success btn-sm"><i class="fas fa-pen"></i></button></div><div class="col-3"><button type="button" id="deactivate'+data+'" onclick="deactivate('+data+')" class="btn btn-danger btn-sm"><i class="fas fa-trash"></i></button></div></div>';
+                }
+            }
+        ],
+        "rowCallback": function( row, data, index ) {
+
+            $('td', row).css('background-color', '#E6E6FA');
+        },        
+    });
+    
+    $.validator.setDefaults({
+        submitHandler: function () {
+            $('#spinnerLoading').show();
+            $.post('php/capacity.php', $('#capacityForm').serialize(), function(data){
+                var obj = JSON.parse(data); 
+                
+                if(obj.status === 'success'){
+                    $('#capacityModal').modal('hide');
+                    toastr["success"](obj.message, "Success:");
+                    
+                    $.get('capacity.php', function(data) {
+                        $('#mainContents').html(data);
+                        $('#spinnerLoading').hide();
+                    });
+                }
+                else if(obj.status === 'failed'){
+                    toastr["error"](obj.message, "Failed:");
+                    $('#spinnerLoading').hide();
+                }
+                else{
+                    toastr["error"]("Something wrong when edit", "Failed:");
+                    $('#spinnerLoading').hide();
+                }
+            });
+        }
+    });
+
+    $('#addCapacity').on('click', function(){
+        $('#capacityModal').find('#id').val("");
+        $('#capacityModal').find('#capacity').val("");
+        $('#capacityModal').modal('show');
+        
+        $('#capacityForm').validate({
+            errorElement: 'span',
+            errorPlacement: function (error, element) {
+                error.addClass('invalid-feedback');
+                element.closest('.form-group').append(error);
+            },
+            highlight: function (element, errorClass, validClass) {
+                $(element).addClass('is-invalid');
+            },
+            unhighlight: function (element, errorClass, validClass) {
+                $(element).removeClass('is-invalid');
+            }
+        });
+    });
+});
+
+function edit(id){
+    $('#spinnerLoading').show();
+    $.post('php/getCapacity.php', {userID: id}, function(data){
+        var obj = JSON.parse(data);
+        
+        if(obj.status === 'success'){
+            $('#capacityModal').find('#id').val(obj.message.id);
+            $('#capacityModal').find('#capacity').val(obj.message.capacity);
+            $('#capacityModal').modal('show');
+            
+            $('#capacityForm').validate({
+                errorElement: 'span',
+                errorPlacement: function (error, element) {
+                    error.addClass('invalid-feedback');
+                    element.closest('.form-group').append(error);
+                },
+                highlight: function (element, errorClass, validClass) {
+                    $(element).addClass('is-invalid');
+                },
+                unhighlight: function (element, errorClass, validClass) {
+                    $(element).removeClass('is-invalid');
+                }
+            });
+        }
+        else if(obj.status === 'failed'){
+            toastr["error"](obj.message, "Failed:");
+        }
+        else{
+            toastr["error"]("Something wrong when activate", "Failed:");
+        }
+        $('#spinnerLoading').hide();
+    });
+}
+
+function deactivate(id){
+    $('#spinnerLoading').show();
+    $.post('php/deleteCapacity.php', {userID: id}, function(data){
+        var obj = JSON.parse(data);
+        
+        if(obj.status === 'success'){
+            toastr["success"](obj.message, "Success:");
+            $.get('capacity.php', function(data) {
+                $('#mainContents').html(data);
+                $('#spinnerLoading').hide();
+            });
+        }
+        else if(obj.status === 'failed'){
+            toastr["error"](obj.message, "Failed:");
+            $('#spinnerLoading').hide();
+        }
+        else{
+            toastr["error"]("Something wrong when activate", "Failed:");
+            $('#spinnerLoading').hide();
+        }
+    });
+}
+</script>
---
+++ php/capacity.php
@@ -0,0 +1,83 @@
+<?php
+require_once "db_connect.php";
+
+session_start();
+
+if(!isset($_SESSION['userID'])){
+    echo '<script type="text/javascript">';
+    echo 'window.location.href = "../login.html";</script>';
+}
+
+if(isset($_POST['capacity'])){
+    $capacity = filter_input(INPUT_POST, 'capacity', FILTER_SANITIZE_STRING);
+
+    if($_POST['id'] != null && $_POST['id'] != ''){
+        if ($update_stmt = $db->prepare("UPDATE `capacity` SET `capacity`=? WHERE id=?")) {
+            $update_stmt->bind_param('ss', $capacity, $_POST['id']);
+            
+            // Execute the prepared query.
+            if (! $update_stmt->execute()) {
+                echo json_encode(
+                    array(
+                        "status"=> "failed", 
+                        "message"=> $update_stmt->error
+                    )
+                );
+            }
+            else{
+                $update_stmt->close();
+                $db->close();
+                
+                echo json_encode(
+                    array(
+                        "status"=> "success", 
+                        "message"=> "Updated Successfully!!" 
+                    )
+                );
+            }
+        }
+    }
+    else{
+        if ($insert_stmt = $db->prepare("INSERT INTO `capacity` (`capacity`) VALUES (?)")) {
+            $insert_stmt->bind_param('s', $capacity);
+            
+            // Execute the prepared query.
+            if (! $insert_stmt->execute()) {
+                echo json_encode(
+                    array(
+                        "status"=> "failed", 
+                        "message"=> $insert_stmt->error
+                    )
+                );
+            }
+            else{
+                $insert_stmt->close();
+                $db->close();
+                
+                echo json_encode(
+                    array(
+                        "status"=> "success", 
+                        "message"=> "Added Successfully!!" 
+                    )
+                );
+            }
+        }
+        else{
+            echo json_encode(
+                array(
+                    "status"=> "failed", 
+                    "message"=> "Something goes wrong when create capacity"
+                )
+            );
+        }
+    }
+}
+else{
+    echo json_encode(
+        array(
+            "status"=> "failed", 
+            "message"=> "Please fill in all the fields"
+        )
+    );
+}
+?>
