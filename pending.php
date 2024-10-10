52159e4bae30a1d5a65c6712440af12bb085e3f4
speedjin1995 <speedjin1995@gmail.com>
2024-10-10 22:00:21 +0800

    fix print borang

--- pending.php
+++ pending.php
@@ -1000,7 +1000,7 @@ $(function () {
 
           // Print button
           buttons += '<div class="col-3"><button title="Print" type="button" id="print'+data+'" onclick="print('+data+
-                    ', '+row.validator+')" class="btn btn-info btn-sm"><i class="fas fa-print"></i></button></div>';
+                    ', \''+row.validator+'\')" class="btn btn-info btn-sm"><i class="fas fa-print"></i></button></div>';
 
           // Complete button if conditions are met
           if (row.stamping_date != '' && row.due_date != '' && row.pin_keselamatan != '' && row.siri_keselamatan != '' && row.borang_d != '') {
@@ -1224,7 +1224,7 @@ $(function () {
 
             // Print button
             buttons += '<div class="col-3"><button title="Print" type="button" id="print'+data+'" onclick="print('+data+
-                      ', '+row.validator+')" class="btn btn-info btn-sm"><i class="fas fa-print"></i></button></div>';
+                    ', \''+row.validator+'\')" class="btn btn-info btn-sm"><i class="fas fa-print"></i></button></div>';
 
             // Complete button if conditions are met
             if (row.stamping_date != '' && row.due_date != '' && row.pin_keselamatan != '' && row.siri_keselamatan != '' && row.borang_d != '') {
