aa3f4380c39a199612f0219b15fc071d6ffc4944
speedjin1995 <speedjin1995@gmail.com>
2025-01-10 23:49:06 +0800

    added new fields

--- cronjob/checkStamping.php
+++ cronjob/checkStamping.php
@@ -30,12 +30,12 @@ try {
     // Prepare insert statement for stamping
     $insertQuery = "INSERT INTO stamping (
         type, dealer, dealer_branch, customer_type, customers, branch, products, brand, machine_type, model, 
-        capacity, capacity_high, assignTo, serial_no, validate_by, jenis_alat, trade, no_daftar, pin_keselamatan, 
-        siri_keselamatan, include_cert, borang_d, borang_e, cawangan, invoice_no, cash_bill, stamping_type, stamping_date, 
+        capacity, capacity_high, assignTo, serial_no, validate_by, jenis_alat, trade, no_daftar, no_daftar_lama, no_daftar_baru, 
+        pin_keselamatan, siri_keselamatan, include_cert, borang_d, borang_e, cawangan, invoice_no, cash_bill, stamping_type, last_year_stamping_date, stamping_date, 
         due_date, pic, customer_pic, quotation_no, quotation_date, purchase_no, purchase_date, remarks, log, 
         unit_price, cert_price, total_amount, sst, subtotal_amount, reason_id, other_reason, existing_id, status, 
         renewed
-    ) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+    ) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
 
     $insertStmt = $pdo->prepare($insertQuery);
 
@@ -43,9 +43,9 @@ try {
     $insertExtQuery = "INSERT INTO stamping_ext (
         stamp_id, penentusan_baru, penentusan_semula, kelulusan_mspk, no_kelulusan, indicator_serial, 
         platform_country, platform_type, size, jenis_pelantar, jenis_penunjuk, alat_type, questions, 
-        nilais, bentuk_dulang, class, batu_ujian, batu_ujian_lain, other_info, load_cell_country, 
-        load_cell_no, load_cells_info
-    ) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+        steelyard, bilangan_kaunterpois, nilais, bentuk_dulang, class, batu_ujian, batu_ujian_lain, other_info, 
+        load_cell_country, load_cell_no, load_cells_info
+    ) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
 
     $insertExtStmt = $pdo->prepare($insertExtQuery);
 
@@ -60,12 +60,11 @@ try {
                 $record['type'], $record['dealer'], $record['dealer_branch'], $record['customer_type'], $record['customers'],
                 $record['branch'], $record['products'], $record['brand'], $record['machine_type'], $record['model'],
                 $record['capacity'], $record['capacity_high'], $record['assignTo'], $record['serial_no'], $record['validate_by'],
-                $record['jenis_alat'], $record['trade'], null, null, null,
-                $record['include_cert'], null, null, $record['cawangan'], null,
-                null, 'RENEWAL', $record['due_date'], null, $record['pic'],
-                $record['customer_pic'], null, null, null, null,
-                $record['remarks'], $record['log'], $record['unit_price'], $record['cert_price'], $record['total_amount'],
-                $record['sst'], $record['subtotal_amount'], $record['reason_id'], $record['other_reason'], $record['id'], 'Pending', 'N'
+                $record['jenis_alat'], $record['trade'], $record['no_daftar_baru'], $record['no_daftar_baru'], null, null, null, 
+                $record['include_cert'], null, null, $record['cawangan'], null, null, 'RENEWAL', $record['stamping_date'], 
+                $record['due_date'], null, $record['pic'], $record['customer_pic'], null, null, null, null, $record['remarks'], 
+                $record['log'], $record['unit_price'], $record['cert_price'], $record['total_amount'], $record['sst'], $record['subtotal_amount'], 
+                $record['reason_id'], $record['other_reason'], $record['id'], 'Pending', 'N'
             ]);
 
             // Get the last inserted ID
@@ -85,8 +84,8 @@ try {
                         $newStampId, $extRecord['penentusan_baru'], $extRecord['penentusan_semula'], $extRecord['kelulusan_mspk'],
                         $extRecord['no_kelulusan'], $extRecord['indicator_serial'], $extRecord['platform_country'], $extRecord['platform_type'],
                         $extRecord['size'], $extRecord['jenis_pelantar'], $extRecord['jenis_penunjuk'], $extRecord['alat_type'],
-                        $extRecord['questions'], $extRecord['nilais'], $extRecord['bentuk_dulang'], $extRecord['class'],
-                        $extRecord['batu_ujian'], $extRecord['batu_ujian_lain'], $extRecord['other_info'], $extRecord['load_cell_country'],
+                        $extRecord['questions'], $extRecord['steelyard'], $extRecord['bilangan_kaunterpois'], $extRecord['nilais'], $extRecord['bentuk_dulang'], 
+                        $extRecord['class'], $extRecord['batu_ujian'], $extRecord['batu_ujian_lain'], $extRecord['other_info'], $extRecord['load_cell_country'],
                         $extRecord['load_cell_no'], $extRecord['load_cells_info']
                     ]);
                 }
