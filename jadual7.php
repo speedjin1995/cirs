3f1628d5df19daa799b56b8268c4d2c028a0ad4a
JihB_0105 <ngjihbin1234@gmail.com>
2025-01-23 14:43:20 +0800

    Fixed Excel Export for all reports

--- jadual7.php
+++ jadual7.php
@@ -615,12 +615,22 @@ $(function () {
   });
 
   $('#exportExcel').on('click', function () {
+    var selectedIds = []; // An array to store the selected 'id' values
+
+    $("#weightTable tbody input[type='checkbox']").each(function () {
+      if (this.checked) {
+        selectedIds.push($(this).val());
+      }
+    });
+
     var fromDateValue = $('#fromDate').val();
     var toDateValue = $('#toDate').val();
     var customerNoFilter = $('#customerNoFilter').val() ? $('#customerNoFilter').val() : '';
     
-    window.open("php/export.php?fromDate="+fromDateValue+"&toDate="+toDateValue+
-    "&customer="+customerNoFilter+"&type=7");
+    if(selectedIds.length > 0){
+      window.open("php/export.php?fromDate="+fromDateValue+"&toDate="+toDateValue+
+      "&customer="+customerNoFilter+"&type=7&id="+selectedIds);
+    }
   });
 });
 
