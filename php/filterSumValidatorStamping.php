e69ecb51b83f9a608dba879db3ef278e61986e6e
speedjin1995 <speedjin1995@gmail.com>
2025-06-11 22:05:06 +0800

    added labour charge

--- php/filterSumValidatorStamping.php
+++ php/filterSumValidatorStamping.php
@@ -28,21 +28,25 @@ if(empty($months)){
   $data = [];
 }else{
   ## Fetch records
-  $empQuery = "SELECT validate_by, DATE_FORMAT(stamping_date, '%b-%y') AS month, SUM(subtotal_amount) AS total_subtotal_amount, SUM(sst) AS total_sst FROM stamping WHERE deleted = 0 AND validate_by IN (9,10)".$searchQuery."and status = 'Complete' GROUP BY validate_by, month ORDER BY validate_by, stamping_date";
+  $empQuery = "SELECT validate_by, DATE_FORMAT(stamping_date, '%b-%y') AS month, SUM(subtotal_amount) AS total_subtotal_amount, SUM(sst) AS total_sst, SUM(labour_charge) AS total_labour FROM stamping WHERE deleted = 0 AND validate_by IN (9,10)".$searchQuery."and status = 'Complete' GROUP BY validate_by, month ORDER BY validate_by, stamping_date";
   $empRecords = mysqli_query($db, $empQuery);
   $data = array();
 
   // Individual Values
   $metroSubTotalCost = array_fill_keys($months, 0);
   $metroSubTotalSST = array_fill_keys($months, 0);
+  $metroSubTotalLabour = array_fill_keys($months, 0);
   $dmetroSubTotalCost = array_fill_keys($months, 0);
   $dmetroSubTotalSST = array_fill_keys($months, 0);
+  $dmetroSubTotalLabour = array_fill_keys($months, 0);
 
   // Sum Values
   $metroSumCost = array_fill_keys($months, 0);
   $metroSumSst = array_fill_keys($months, 0);
+  $metroSumLabour = array_fill_keys($months, 0);
   $dmetroSumCost = array_fill_keys($months, 0);
   $dmetroSumSst = array_fill_keys($months, 0);
+  $dmetroSumLabour= array_fill_keys($months, 0);
 
   while ($row = mysqli_fetch_assoc($empRecords)) {
     $monthYear = $row['month'];
@@ -50,13 +54,17 @@ if(empty($months)){
     if ($row['validate_by'] == 10) {
       $metroSumCost[$monthYear] = (float) $row['total_subtotal_amount'];
       $metroSumSst[$monthYear] = (float) $row['total_sst'];
+      $metroSumLabour[$monthYear] = (float) ($row['total_labour'] ?? '0.00');
       $metroSubTotalCost[$monthYear] = "RM " . number_format((float) $row['total_subtotal_amount'], 2);
       $metroSubTotalSST[$monthYear] = "RM " . number_format((float) $row['total_sst'], 2);
+      $metroSubTotalLabour[$monthYear] = "RM " . number_format((float) ($row['total_labour'] ?? '0.00'), 2);
     } elseif ($row['validate_by'] == 9) {
       $dmetroSumCost[$monthYear] = (float) $row['total_subtotal_amount'];
       $dmetroSumSst[$monthYear] = (float) $row['total_sst'];
+      $dmetroSumLabour[$monthYear] = (float) ($row['total_labour'] ?? '0.00');
       $dmetroSubTotalCost[$monthYear] = "RM " . number_format((float) $row['total_subtotal_amount'], 2);
       $dmetroSubTotalSST[$monthYear] = "RM " . number_format((float) $row['total_sst'], 2);
+      $dmetroSubTotalLabour[$monthYear] = "RM " . number_format((float) ($row['total_labour'] ?? '0.00'), 2);
     }
   }
 
@@ -78,20 +86,28 @@ if(empty($months)){
   // filter arrays for consistency with $months
   $metroSumCost = array_intersect_key($metroSumCost, array_flip($metroMonths));
   $metroSumSst = array_intersect_key($metroSumSst, array_flip($metroMonths));
+  $metroSumLabour = array_intersect_key($metroSumLabour, array_flip($metroMonths));
   $metroSubTotalCost = array_intersect_key($metroSubTotalCost, array_flip($metroMonths));
   $metroSubTotalSST = array_intersect_key($metroSubTotalSST, array_flip($metroMonths));
+  $metroSubTotalLabour = array_intersect_key($metroSubTotalLabour, array_flip($metroMonths));
+
   $dmetroSumCost = array_intersect_key($dmetroSumCost, array_flip($dmetroMonths));
   $dmetroSumSst = array_intersect_key($dmetroSumSst, array_flip($dmetroMonths));
+  $dmetroSumLabour = array_intersect_key($dmetroSumLabour, array_flip($dmetroMonths));
   $dmetroSubTotalCost = array_intersect_key($dmetroSubTotalCost, array_flip($dmetroMonths));
   $dmetroSubTotalSST = array_intersect_key($dmetroSubTotalSST, array_flip($dmetroMonths));
+  $dmetroSubTotalLabour = array_intersect_key($dmetroSubTotalLabour, array_flip($dmetroMonths));
 
   // Create data rows
   $data[] = array_merge(["column_1" => "Metrology"], array_combine(array_slice($columns, 1, -1), $months), ["last_column" => "Sub Total"]);
   $data[] = array_merge(["column_1" => "Sub Total Cost"], array_combine(array_slice($columns, 1, -1), array_values($metroSubTotalCost)), ["last_column" => "RM " . number_format((float) array_sum($metroSumCost), 2)]);
   $data[] = array_merge(["column_1" => "Sub Total SST"], array_combine(array_slice($columns, 1, -1), array_values($metroSubTotalSST)), ["last_column" => "RM " . number_format((float) array_sum($metroSumSst), 2)]);
+  $data[] = array_merge(["column_1" => "Sub Total Labour"], array_combine(array_slice($columns, 1, -1), array_values($metroSubTotalLabour)), ["last_column" => "RM " . number_format((float) array_sum($metroSumLabour), 2)]);
+
   $data[] = array_merge(["column_1" => "DE Metrology"], array_combine(array_slice($columns, 1, -1), $months), ["last_column" => "Sub Total"]);
   $data[] = array_merge(["column_1" => "Sub Total Cost"], array_combine(array_slice($columns, 1, -1), array_values($dmetroSubTotalCost)), ["last_column" => "RM " . number_format((float) array_sum($dmetroSumCost), 2)]);
   $data[] = array_merge(["column_1" => "Sub Total SST"], array_combine(array_slice($columns, 1, -1), array_values($dmetroSubTotalSST)), ["last_column" => "RM " . number_format((float) array_sum($dmetroSumSst), 2)]);
+  $data[] = array_merge(["column_1" => "Sub Total Labour"], array_combine(array_slice($columns, 1, -1), array_values($dmetroSubTotalLabour)), ["last_column" => "RM " . number_format((float) array_sum($dmetroSumLabour), 2)]);
 }
 
 ## Response
